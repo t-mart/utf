650d9018d43fd94f7369ca4a4b02400bbaf2006e
tim <tim@timmart.in>
2019-09-20 10:43:09 -0500

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,46 @@
 from setuptools import setup, find_packages
+import codecs
+import sys
 
-setup(name="utf", packages=find_packages())
+from setuptools.command.test import test as TestCommand
+
+import utf
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+def long_description():
+    with codecs.open("README.md", encoding="utf8") as f:
+        return f.read()
+
+
+setup(
+    name="utf",
+    version=utf.__version__,
+    packages=find_packages(),
+    url="https://github.com/t-mart/utf",
+    download_url="https://github.com/t-mart/utf",
+    license=utf.__licence__,
+    author=utf.__author__,
+    author_email="tim@timmart.in",
+    description=utf.__doc__.strip(),
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    cmdclass={"test": PyTest},
+    tests_require=["pytest"],
+)
--- utf/__init__.py
+++ utf/__init__.py
@@ -1,3 +1,10 @@
+"""
+utf - Just Unicode things
+"""
+__version__ = "0.0.1"
+__author__ = "Tim Martin"
+__licence__ = "MIT"
+
 from .utf8 import encode_utf8, decode_utf8
 from .utf16 import encode_utf16be, encode_utf16le, decode_utf16be, decode_utf16le
 from .utf32 import encode_utf32be, encode_utf32le, decode_utf32be, decode_utf32le
